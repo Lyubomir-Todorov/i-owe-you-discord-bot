a82a1e963a909133538d0647812d8e15685dfb00
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-04-15 15:06:50 -0400

    Change service account environment variable from file path to variables for client email and private key

--- src/spreadsheet/spreadsheet.ts
+++ src/spreadsheet/spreadsheet.ts
@@ -2,11 +2,9 @@ import { JWT } from "google-auth-library";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import { config } from "../config";
 
-const credentials = require(config.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS_PATH);
-
 const serviceAccountAuth = new JWT({
-    email: credentials.client_email,
-    key: credentials.private_key,
+    email: config.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    key: config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
