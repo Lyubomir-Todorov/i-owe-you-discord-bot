998dd7b7d560249faed9b8315886db957c1885cb
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-09 15:11:58 -0400

    Refactor WorksheetConfiguration type definition and add documentation

--- src/types/worksheet-configuration.ts
+++ src/types/worksheet-configuration.ts
@@ -1,9 +1,27 @@
 import { Person } from "./person";
 import { PurchaseCategory } from "./purchase-category";
 
+/**
+ * WorksheetConfiguration type.
+ * Represents the configuration settings in the worksheet.
+ */
 export type WorksheetConfiguration = {
+    /**
+     * Configuration properties for the two people involved in the spreadsheet.
+     * @see Person
+     */
     people: Person[];
+    /**
+     * The available purchase categories that a purchase can belong to.
+     * @see PurchaseCategory
+     */
     purchaseCategories: PurchaseCategory[];
+    /**
+     * The name of the default purchase category, in the case no category or category keywords are mentioned.
+     */
     defaultPurchaseCategory: string;
+    /**
+     * Filler words that can be ignored when parsing the message.
+     */
     blacklistedKeywords: string[];
 };
