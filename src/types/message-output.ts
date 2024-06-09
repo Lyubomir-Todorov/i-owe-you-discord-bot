7e8f51d75420fd8715c8a042a8ee1741f5f5c659
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-09 15:08:50 -0400

    Refactor MessageOutput type definition and add documentation

--- src/types/message-output.ts
+++ src/types/message-output.ts
@@ -1,6 +1,23 @@
+/**
+ * MessageOutput type. The resulting output after parsing the message.
+ */
 export type MessageOutput = {
+    /**
+     * The amount spent.
+     */
     amount: string;
+    /**
+     * The category of the purchase.
+     */
     category: string;
+    /**
+     * The description of the purchase.
+     * This is usually the item or service purchased.
+     */
     description: string;
+    /**
+     * The purchaser of the purchase.
+     * This maps to the name of the person who made the purchase.
+     */
     purchaser: "person1" | "person2";
 };
