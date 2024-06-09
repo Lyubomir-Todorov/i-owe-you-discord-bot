e9bc6fe7ed7cf549bef7add033841867a03c976a
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-09 15:07:48 -0400

    Improve Person type definition and add documentation

--- src/types/person.ts
+++ src/types/person.ts
@@ -1,5 +1,19 @@
+/**
+ * Person, represents one of the users in the spreadsheet.
+ */
 export type Person = {
+    /**
+     * The name of the person.
+     */
     name: string;
+    /**
+     * The Discord ID of the person.
+     * This is used to correctly identify the purchaser if no name is mentioned in the message.
+     */
     discordId: string;
+    /**
+     * The aliases / nicknames of the person.
+     * This is used to correctly identify the purchaser if an alias is mentioned in the message.
+     */
     aliases: string[];
 };
