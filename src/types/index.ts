ac50d6f2404bd59b94ca475a9a6da22bc7301110
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-18 19:44:42 -0400

    Export recurring purchase type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export * from "./purchase-category";
 export * from "./worksheet-configuration";
 export * from "./message-output";
 export * from "./purchaser";
+export * from "./recurring-purchase";
