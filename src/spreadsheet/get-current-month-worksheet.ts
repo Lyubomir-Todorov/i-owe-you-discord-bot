348809bfa65ba2d1177655d79d9f2fd2a480fc60
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-18 19:11:39 -0400

    Allow month worksheets to be named in shorthand notation

--- src/spreadsheet/get-current-month-worksheet.ts
+++ src/spreadsheet/get-current-month-worksheet.ts
@@ -2,13 +2,15 @@ import { spreadsheet } from "./spreadsheet";
 
 export async function getCurrentMonthWorksheet() {
     const date = new Date();
-    const month = date.toLocaleString("default", { month: "long" });
+    const monthLong = date.toLocaleString("default", { month: "long" });
+    const monthShort = date.toLocaleString("default", { month: "short" });
 
-    const worksheetCurrentMonth = spreadsheet.sheetsByTitle[month];
+    const worksheetCurrentMonth =
+        spreadsheet.sheetsByTitle[monthShort] ||
+        spreadsheet.sheetsByTitle[monthLong];
 
-    if (!worksheetCurrentMonth) {
-        throw new Error(`Worksheet for ${month} was not found`);
-    }
+    if (!worksheetCurrentMonth)
+        throw new Error(`Worksheet for ${monthLong} was not found`);
 
     return worksheetCurrentMonth;
 }
