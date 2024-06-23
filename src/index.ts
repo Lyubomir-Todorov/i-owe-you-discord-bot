4dfe127545611bf6f95b197b1e79d14930d007e8
Lyubomir Todorov <leo.todorov@hotmail.com>
2024-06-23 12:42:25 -0400

    Finish logic implementation of adding recurring purchases to current month's worksheet. Consolidate logic into one method and add channel message output

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { CronJob } from "cron";
+import {
+    Client,
+    GatewayIntentBits,
+    HeadingLevel,
+    heading
+} from "discord.js";
 import { commands } from "./commands";
 import { config } from "./config";
 import { deployCommands } from "./deploy-commands";
 import { parseMessageIntoPurchase } from "./parse-message-into-purchase";
 import { initializeSpreadsheet } from "./spreadsheet";
-import { CronJob } from "cron";
 import processRecurringPayments from "./spreadsheet/recurring-payments/process-recurring-payments";
 
 const job = CronJob.from({
     cronTime: config.RECURRING_WORKSHEET_CRON,
     onTick: async function () {
-        await processRecurringPayments();
+        const purchases = await processRecurringPayments();
+        if (purchases.length === 0) {
+            return;
+        }
+
+        const currentMonth = new Date().toLocaleString("default", {
+            month: "long",
+        });
+
+        var message = heading(
+            `The following recurring purchases were added to ${currentMonth}'s worksheet: \n`,
+            HeadingLevel.Three
+        );
+        const delimitedPurchases = purchases
+            .map((x) => {
+                return `* A ${x.frequency} \$${x.amount} purchase for ${x.description} under ${x.category}, made by ${x.purchaser}`;
+            })
+            .join("\n");
+
+        message += delimitedPurchases;
+
+        client.channels.cache.forEach((channel) => {
+            if (channel.isTextBased()) channel.send(message);
+        });
     },
     start: false,
 });
--- src/spreadsheet/recurring-payments/process-recurring-payments.ts
+++ src/spreadsheet/recurring-payments/process-recurring-payments.ts
@@ -1,12 +1,103 @@
+import { differenceInDays } from "date-fns";
 import { config } from "src/config";
+import { insertNewPurchase } from "src/insert-new-purchase";
+import { RecurringPurchase, RecurringPurchaseFrequency } from "src/types";
+import { getConfiguration } from "../configuration";
 import getWorksheet from "../get-worksheet";
-import getRecurringPayments from "./get-recurring-payments";
 
-export default async function processRecurringPayments() {
+export default async function processRecurringPayments(): Promise<
+    RecurringPurchase[]
+> {
+    console.info(
+        `Starting processing recurring payments at ${new Date().toISOString()}...`
+    );
     const worksheet = await getWorksheet(config.RECURRING_WORKSHEET_NAME);
-    await worksheet.loadCells("A1:Z1000");
+    const configuration = await getConfiguration();
 
-    await getRecurringPayments(worksheet);
+    const purchaseAmountRegExp = new RegExp(/\d+(\.\d+)?/);
+
+    await worksheet.loadHeaderRow(
+        Number(config.RECURRING_WORKSHEET_ROW_OFFSET) - 1
+    );
+
+    const successfulRecurringPayments: RecurringPurchase[] = [];
+    var rows = await worksheet.getRows();
+    for (const row of rows) {
+        const recurringPurchase: RecurringPurchase = {
+            enabled: row.get("Disabled") === "FALSE",
+            description: String(row.get("Description")).trim(),
+            amount: Number(purchaseAmountRegExp.exec(row.get("Amount"))?.[0]),
+            purchaser: String(row.get("Purchaser")).trim(),
+            category: row.get("Category"),
+            frequency: row.get("Frequency") as RecurringPurchaseFrequency,
+            lastRunDate: row.get("Last time added")
+                ? new Date(row.get("Last time added"))
+                : null,
+        };
+
+        const {
+            enabled,
+            description,
+            amount,
+            purchaser,
+            category,
+            frequency,
+            lastRunDate,
+        } = recurringPurchase;
+
+        if (!enabled) {
+            console.log(`Purchase ${description} is disabled, skipping...`);
+            continue;
+        }
+
+        if (!description || !amount || !purchaser || !category || !frequency) {
+            continue;
+        }
+
+        const purchaserAsPosition = configuration.people.find(
+            (x) => x.name === purchaser
+        )?.position;
+
+        if (!purchaserAsPosition) {
+            console.error(`Person ${purchaser} not found in the configuration`);
+            continue;
+        }
+
+        const descriptionWithRecurringLabel =
+            "#RECURRING PURCHASE# " + description;
+
+        const purchaseNeverMade = !lastRunDate;
+        const purchaseDue = lastRunDate
+            ? differenceInDays(new Date(), lastRunDate) >=
+              Number(RecurringPurchaseFrequency[frequency])
+            : false;
+
+        if (!purchaseNeverMade && !purchaseDue) {
+            console.log(`Purchase ${description} is not due, skipping...`);
+            continue;
+        }
+
+        await insertNewPurchase({
+            description: descriptionWithRecurringLabel,
+            category,
+            purchaser: purchaserAsPosition,
+            amount: String(amount),
+        });
+
+        row.assign({
+            "Last time added": new Date(),
+            Purchaser: purchaser.trim(),
+        });
+
+        row.save();
+
+        console.log(
+            `Successfully added purchase ${description} to the purchases sheet`
+        );
+
+        successfulRecurringPayments.push(recurringPurchase);
+    }
 
     worksheet.resetLocalCache(true);
+    return successfulRecurringPayments;
 }
